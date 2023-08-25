53dc05d1365625a60c4db15ed8e6076dc066b52f
luka2210 <lukamomcilovic221099@gmail.com>
2023-08-25 22:59:54 +0200

    Improved randomness of function that picks mine locations.

--- MinesweeperSolver/src/gameutil/RandomUniqueNumbers.java
+++ MinesweeperSolver/src/gameutil/RandomUniqueNumbers.java
@@ -2,11 +2,12 @@ package gameutil;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Random;
 
 public class RandomUniqueNumbers {
 	
 	public static ArrayList<Integer> generate(int size, int n, ArrayList<Integer> numAvoid) {
-		ArrayList<Integer> allNumbers = new ArrayList<Integer>(size - 1);
+		ArrayList<Integer> allNumbers = new ArrayList<Integer>();
 		for (int i = 0; i < size; i++)
 			if (!numAvoid.contains(i))
 				allNumbers.add(i);
@@ -14,8 +15,12 @@ public class RandomUniqueNumbers {
 		Collections.shuffle(allNumbers);
 		
 		ArrayList<Integer> randomNumbers = new ArrayList<Integer>(n);
-		for (int i = 0; i < n; i++)
-			randomNumbers.add(allNumbers.get(i));
+		Random random = new Random();
+		for (int i = 0; i < n; i++) {
+			int index = random.nextInt(allNumbers.size());
+			randomNumbers.add(allNumbers.get(index));
+			allNumbers.remove(index);
+		}
 		
 		return randomNumbers;
 	}
